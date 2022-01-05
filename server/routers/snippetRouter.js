8409067da008647aa05dbd68853a351539f9797b
Yonatan Haddish <yonihailshn@gmail.com>
2022-01-05 16:35:12 -0500

    update router created

--- server/routers/snippetRouter.js
+++ server/routers/snippetRouter.js
@@ -53,6 +53,36 @@ router.delete("/:id", async (req,res) => {
     catch(err) {
         res.status(500).send();
     }
+});
+
+// update a snippet by a specific Id
+router.put("/:id", async (req, res) => {
+    try {
+        const {title, description,code}= req.body;
+        const snippetId= req.params.id;
+
+        if (!description && !code) {
+            return res.status(400).json({errorMessage: "You need to enter at least a description or code"});
+        }
+        
+        if (!snippetId)
+            return res.status(400).json({errorMessage: "Snippet Id not given. Please contact the developer"});
+
+        const originalSnippet= await Snippet.findById(snippetId);
+        if (!originalSnippet)
+            return res.status(400).json({errorMessage: "No snippet with this Id was found. Please contact the developer"});
+        
+        originalSnippet.title= title;
+        originalSnippet.description= description;
+        originalSnippet.code= code;
+
+        const savedSnippet= await originalSnippet.save();
+
+        res.json(savedSnippet);
+    }
+    catch(err) {
+        res.status(500).send();
+    }
 })
 
 
