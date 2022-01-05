2cb2d3d94bddc38681e2e6c3de8a57ac6f7f51c3
Yonatan Haddish <yonihailshn@gmail.com>
2022-01-05 16:05:27 -0500

    delete and findOneById created

--- server/routers/snippetRouter.js
+++ server/routers/snippetRouter.js
@@ -1,6 +1,18 @@
 const router= require("express").Router();
 const Snippet= require("../models/snippetModel");
 
+// get all snippets
+router.get("/", async (req, res) => {
+    try {
+        const snippets= await Snippet.find();
+        res.json(snippets);
+    }
+    catch(err) {
+        res.status(500).send();
+    }
+});
+
+// post snippets
 router.post("/", async (req, res) => {
     try {
         const {title, description, code}= req.body;
@@ -20,5 +32,30 @@ router.post("/", async (req, res) => {
         res.status(500).send();
     }
 });
+
+// delete snippet by a specific Id
+router.delete("/:id", async (req,res) => {
+    try {
+        const snippetId= req.params.id;
+        
+        if (!snippetId)
+            return res.status(400).json({errorMessage: "Snippet Id not given. Please contact the developer."});
+        
+        const existingSnippet= await Snippet.findById(snippetId);
+
+        if (!existingSnippet)
+            return res.status(400).json({errorMessage: "No snippet with this Id was found. Please contact the developer"})
+
+        await existingSnippet.delete();
+        
+        res.json(existingSnippet);
+    }
+    catch(err) {
+        res.status(500).send();
+    }
+})
+
+
+
 
 module.exports= router; 
