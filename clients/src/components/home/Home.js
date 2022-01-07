f8af57a73d6230c4a91820e028d93ef6bc956483
Yonatan Haddish <yonihailshn@gmail.com>
2022-01-07 15:19:27 -0500

    form created on toggle function

--- clients/src/components/home/Home.js
+++ clients/src/components/home/Home.js
@@ -1,30 +1,51 @@
 import React, { useState, useEffect } from "react";
 import Snippet from "./Snippet";
-const Axios= require('axios');
-
+const Axios = require("axios");
 
 function Home() {
+  const [snippets, setSnippets] = useState([]);
+  const [newSnippetEditorOpen, setNewSnippetEditorOpen] = useState(false);
 
-    const [snippets, setSnippets]= useState([]);
+  useEffect(() => {
+    getSnippet();
+  }, []);
 
-    useEffect(() => {
-        getSnippet();
-    }, []);
+  async function getSnippet() {
+    const snippetsRes = await Axios.get("http://localhost:5000/snippet/");
+    setSnippets(snippetsRes.data);
+  }
 
-    async function getSnippet() {
-        const snippetsRes= await Axios.get("http://localhost:5000/snippet/");
-        setSnippets(snippetsRes.data);
-    }
+  function renderSnippets() {
+    return snippets.map((snippet, i) => {
+      return <Snippet snippet={snippet} key={i} />;
+    });
+  }
 
-    function renderSnippets() {
-        return snippets.map((snippet, i) => {
-            return <Snippet snippet= {snippet} key={i}/>
-        })
-    }
+  return (
+    <div className="home">
+      {!newSnippetEditorOpen && (
+        <button onClick={() => setNewSnippetEditorOpen(true)}>
+          Add Snippet
+        </button>
+      )}
+      {newSnippetEditorOpen && (
+        <div>
+          <form>
+            <label htmlFor="editor-title">Title</label>
+            <input id="editor-title" />
 
-    return (
-        <div className="home">{renderSnippets()}</div>
-    )
+            <label htmlFor="editor-description">Desctiption</label>
+            <input id="editor-description" />
+
+            <label htmlFor="editor-code">Code</label>
+            <textarea id="editor-code" />
+          </form>
+        </div>
+      )}
+
+      {renderSnippets()}
+    </div>
+  );
 }
 
 export default Home;
