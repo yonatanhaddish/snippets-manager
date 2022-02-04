fc7a09e7a33734df7c8a8ef051695ad3ceee0302
Yonatan Haddish <yonihailshn@gmail.com>
2022-02-04 11:08:29 -0500

    middleware created for token

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express= require('express');
 const mongoose= require('mongoose');
 const dotenv= require("dotenv");
 const cors= require('cors');
+const cookieParser= require('cookie-parser');
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(cors({
     origin: "http://localhost:3000"
 }));
+app.use(cookieParser());
 
 app.listen(5000, () => console.log("Server started on port 5000"));
 
