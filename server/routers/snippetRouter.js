c5f1b540d3e4f0111c07e14a8f31635d4f842368
Yonatan Haddish <yonihailshn@gmail.com>
2022-02-07 18:19:33 -0500

    delete and updating auth implemented

--- server/routers/snippetRouter.js
+++ server/routers/snippetRouter.js
@@ -25,12 +25,7 @@ router.post("/", auth, async (req, res) => {
         const newSnippet= new Snippet({
             title, description, code, user: req.user
         });
-        
-        // console.log(title);
-        // console.log(description);
-        // console.log(code);
-        // console.log("user");
-        // console.log(newSnippet);
+
         const savedSnippet= await newSnippet.save();
 
         res.json(savedSnippet);
@@ -41,7 +36,7 @@ router.post("/", auth, async (req, res) => {
 });
 
 // delete snippet by a specific Id
-router.delete("/:id", async (req,res) => {
+router.delete("/:id", auth, async (req,res) => {
     try {
         const snippetId= req.params.id;
         
@@ -66,7 +61,7 @@ router.delete("/:id", async (req,res) => {
 });
 
 // update a snippet by a specific Id
-router.put("/:id", async (req, res) => {
+router.put("/:id", auth, async (req, res) => {
     try {
         const {title, description,code}= req.body;
         const snippetId= req.params.id;
